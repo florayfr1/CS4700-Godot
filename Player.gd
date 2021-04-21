e7d749bbcd60ab94057b0abe130cf3b5cfd0374f
florayfr1 <florayfr@gmail.com>
2021-04-20 22:58:48 -0700

    part 1

--- Player.gd
+++ Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var acceleration = 500
+var friction = 200
 
 func _physics_process(delta):
 	if Input.is_action_pressed("move_left"):
@@ -9,3 +10,5 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("move_right"):
 		velocity.x = +acceleration
 	move_and_slide(velocity)
+
+	velocity = velocity.move_toward(Vector2.ZERO, friction)
