22634fb499d47237f5f9185e10bb88afa2b7586a
florayfr1 <florayfr@gmail.com>
2021-04-22 00:54:08 -0700

    Part 3

--- Player'/Ball.gd
+++ Player'/Ball.gd
@@ -1,13 +1,30 @@
 extends KinematicBody2D
 
-var speed = 500
+export var speed = 500
+var world = "res://Game 1/MainScene.tscn"
 var direction = Vector2(0.5, 1)
+var is_throwing = false
+
+onready var is_visible = get_node("BallVisible")
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("throw"):
+		is_throwing = true
+	
+	if not is_throwing:
+		return
+	
+	is_game_over()
+	
 	var velocity = speed * direction * delta
 	var collision = move_and_collide(velocity)
 	
 	if collision != null:
 		direction = direction.bounce(collision.normal)
 		
-	
+
+func is_game_over():
+	if not is_visible.is_on_screen():
+		print("Game over")
+		get_tree().change_scene(world)
+		
