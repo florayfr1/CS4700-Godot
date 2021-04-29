defc70fd4e008c31116ab005b28dabe436557989
emilyrvillalba <emilyrvillalba@gmail.com>
2021-04-28 21:57:26 -0700

    Dog

---
+++ Game_improve/Game_Classic/Bricks/Brick.gd
@@ -0,0 +1,56 @@
+extends StaticBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var points
+
+onready var animated_sprite = get_node("AnimatedSprite")
+
+var blue_frames = preload("res://Game_Classic/Bricks/frames/brick1_frame.tres")
+var green_frames = preload("res://Game_Classic/Bricks/frames/brick2_frame.tres")
+var grey_frames = preload("res://Game_Classic/Bricks/frames/brick0_frame.tres")
+
+enum Frames {BLUE = 1, GREEN = 2, GREY = 0}
+
+var current_frames = Frames.BLUE
+
+var indestructable = false
+
+func set_frames(frames):
+	current_frames = frames
+
+func change_frames():
+	match current_frames:
+		Frames.BLUE:
+			animated_sprite.frames = blue_frames
+		Frames.GREEN:
+			animated_sprite.frames = green_frames
+		Frames.GREY:
+			animated_sprite.frames = grey_frames
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_meta("brick", true)
+	change_frames()
+	points = animated_sprite.frames.get_frame_count("default")
+	
+func set_indestructable(booleanValue):
+	indestructable = booleanValue
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func decrease_points():
+	if indestructable:
+		return
+	
+	points -= 1
+	
+	if animated_sprite.frame < animated_sprite.frames.get_frame_count("default")  -1:
+		animated_sprite.frame += 1
+	
+	if points <= 0:
+		queue_free()
---
+++ Game_improve/Game_Classic/Player/Player.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var acceleration = 500
+var friction = 200
+
+export var player_length = 7
+
+onready var player_map = get_node("player_map")
+const p_container = preload("res://Game_Classic/Player/PContainer.tres")
+#player_container
+const player_cell_width = 32  #width of indiv player cell in pixels
+
+func _ready():
+	build_player(player_length)
+	p_container.player = self
+	p_container.player_width = player_length * player_cell_width
+func build_player(length):
+	assert(length >= 2, "Error: length must be greater than 2")
+	#Video part 4
+	var temp_cell = 0
+	var left_body_part = 1
+	var middle_body_part = 3
+	var right_body_part = 2
+	
+	player_map.set_cell(temp_cell,0,left_body_part)
+	
+	for i in range (0, length - 1):
+		temp_cell += 1
+		player_map.set_cell(temp_cell,0,middle_body_part)
+		
+	temp_cell += 1
+	player_map.set_cell(temp_cell,0,right_body_part)
+	
+func _physics_process(delta):
+	fix_y()
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -acceleration
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = +acceleration
+	move_and_slide(velocity)
+
+	velocity = velocity.move_toward(Vector2.ZERO, friction)
+
+func fix_y():
+	position.y = 665
